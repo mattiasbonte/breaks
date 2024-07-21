d5c4c688ed5b18c53da3b9545c0b72e5d2872a53
Mattias B <info@mattiasbonte.dev>
2024-07-21 17:03:42 +0200

    feat(func): add main functionality for first timer script

--- main.go
+++ main.go
@@ -3,45 +3,72 @@ package main
 import (
 	"fmt"
 	"os/exec"
+	"strconv"
 	"time"
 )
 
+const tempFile = "/tmp/user_idle_state"
+
+// --
+// Creates a temp file if the user is idle
+// Removes that file if the user is active
+// --
+func handleActivityState(treshold int) {
+	_, err := exec.Command("rm", tempFile).Output()
+	if err != nil {
+		fmt.Println("Error deleting file", err)
+	}
+
+	cmd := exec.Command(
+		"swayidle",
+		"-w",
+		"timeout",
+		strconv.Itoa(treshold),
+		"echo 'idle' > "+tempFile,
+		"resume",
+		"rm "+tempFile,
+	)
+	cmd.Run()
+}
+
+// --
+// Sends a notification to the user
+// --
 func sendNotification(message string) error {
 	cmd := exec.Command("notifyme", "-p", "-m", message)
 	return cmd.Run()
 }
 
 func main() {
+	// --
+	// SETTINGS change to user preference
+	// --
+	notifyAfterMin := 30      // notify user to break after X min
+	pollTimeSeconds := 60     // the time between each poll, and therefor notification
+	idleTimeSeconds := 3 * 60 // 5min is considered idle
+
+	// run the idle checker in the background
+	go handleActivityState(idleTimeSeconds)
+
+	// the total time the user has been active
+	secondsActive := 0
 	for {
-		// Create the swayidle command
-		cmd := exec.Command("swayidle", "-w", "timeout", "60", "echo 'idle'")
+		_, err := exec.Command("cat", tempFile).Output()
+		if err != nil {
+			secondsActive += pollTimeSeconds
+			fmt.Println("😅 User active for", secondsActive/60, "min")
+		} else {
+			secondsActive = 0
+			fmt.Println("💤 User idle for", idleTimeSeconds/60, "min")
+		}
 
-		// Use a channel to capture the 'idle' output or termination.
-		done := make(chan error, 1)
-		go func() {
-			err := cmd.Run()
-			done <- err // Send nil if no error occurs, else send error
-		}()
-
-		select {
-		case err := <-done:
+		if secondsActive >= notifyAfterMin*60 {
+			err := sendNotification("🚨 Please Pause! (" + strconv.Itoa(secondsActive/60) + " min active)")
 			if err != nil {
-				fmt.Println("Error running swayidle:", err)
-				return
-			}
-			// User was idle, swayidle completed, wait for activity
-			for {
-				// Wait until the user gets active
-				time.Sleep(1 * time.Second)
-				cmdCheckActive := exec.Command("swaymsg", "-t", "get_tree")
-				err = cmdCheckActive.Run()
-				if err == nil {
-					// User is active
-					time.Sleep(1 * time.Minute)
-					sendNotification("You have been active for more than 1 minute!")
-					break
-				}
+				fmt.Println("🔴 Error sending notification", err)
 			}
 		}
+
+		time.Sleep(time.Duration(pollTimeSeconds) * time.Second)
 	}
 }
